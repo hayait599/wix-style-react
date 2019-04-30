f9f35f517dd8272464831a280d5608286d1aaf9a
TalZa <talza@wix.com>
2019-04-30 11:53:35 +0300

    removing the new component generated and transferring the files to the existed directory. migrating the unit tests from wix-ui-backoffice to WSR

--- src/LinearProgressBar/test/storySettings.js
+++ src/LinearProgressBar/test/storySettings.js
@@ -1,10 +1,11 @@
 import { Category } from '../../../stories/storiesHierarchy';
 
+//TODO: changing testStoryName according to e2e
 export const storySettings = {
   category: Category.COMPONENTS,
   storyName: 'LinearProgressBar',
   dataHook: 'story-linear-progress-bar',
   testStoryNames: {
-    DEFAULT: 'default',
+    DEFAULT: 'Default',
   },
 };
